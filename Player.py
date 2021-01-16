5fb2615df9e069b402e01209d12babc3586e03e1
DaErich <daerich@pop-os.localdomain>
2021-01-16 21:15:03 +0100

    Added OOP features

---
+++ Player.py
@@ -0,0 +1,60 @@
+import mojangapi
+import base64;
+import aiohttp
+
+#OOP in Wrapper
+
+class Player():
+
+    def create(username):
+        self = Player()
+        self.username= username
+        self.uuid = None
+        self.skinURI = None
+        return self
+
+    async def get_uuid(self):
+        if self.uuid == None: #Caching
+            resp = await mojangapi.perform_request( mojangapi.KnownURIs.uuidURI + self.username)
+            self.uuid = resp["id"]
+            return self.uuid
+        else:
+            return self.uuid
+
+    async def get_profile(self,decode = False):
+        uuid = await self.get_uuid()
+        resp =  await mojangapi.perform_request(mojangapi.KnownURIs.sessionServer + uuid)
+        if decode:
+            resp["properties"][0]["value"] = base64.b64decode(resp["properties"][0]["value"])
+            return resp
+        else:
+            return resp
+    async def get_names(self):
+        uuid = await self.get_uuid()
+        return await mojangapi.perform_request(mojangapi.KnownURIs.MojangAccUUID + uuid + "/names")
+
+    async def download_skin(self):
+        async with aiohttp.ClientSession() as session:
+            skin = await self.get_skin()
+            async with session.get(skin) as res:
+                if res.status == 200:
+                
+                    with open('download.png', 'wb') as file:
+                        while True:
+                            chunk = await res.content.read(500)
+                            if not chunk:
+                                print("Download finished!")
+                                break
+                            else:
+                                file.write(chunk)
+        
+                else:
+                    raise LookupError("LookupFailed")
+
+    async def get_skin(self):
+        if self.skinURI == None: #Caching
+            uuid = await self.get_uuid()
+            decoded = await self.get_profile(True)
+            self.skinURI = eval(decoded["properties"][0]["value"].decode("ascii"))["textures"]["SKIN"]["url"]
+        else:
+            return self.skinURI
---
+++ main_oop.py
@@ -0,0 +1,39 @@
+import asyncio
+import aiohttp
+import mojangapi
+import Player
+from argparse import ArgumentParser
+
+# Initialize argsparser
+parser = ArgumentParser(description="Leverages Mojangs API -- Will execute only switch (in order of appearance)")
+parser.add_argument("Username", help="Minecraft username")
+parser.add_argument("--uuid", action="store_true", help="Prints uuid and exits")
+parser.add_argument("--history", action="store_true", help="See account name history")
+parser.add_argument("--session",action="store_true", help="Prints sessioninformation and exits")
+parser.add_argument("--decode",action="store_true", help="decode base64 encrypted contents")
+parser.add_argument("--skin", action="store_true", help="Download skin to current folder")
+args=parser.parse_args()
+#EO argsparser
+
+
+async def main():
+    if args.Username != None:
+            myPlayer = Player.Player.create(args.Username)
+            if args.uuid:
+                print(await myPlayer.get_uuid())
+                return
+            if args.session:
+                print(await myPlayer.get_profile())
+                return
+            if args.skin:
+                await myPlayer.download_skin()
+                return
+            if args.history:
+                print(await myPlayer.get_names())
+                return
+            else:
+                print("Please use -h switch")
+    else:
+        print("Please enter a valid username!")
+
+asyncio.run(main())
